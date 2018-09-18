60aff2a40b1ca9dcfcf622055be98b7330176ffc
Gus <gdiazbanuelos@gmail.com>
2018-09-18 16:14:58 -0500

    Implenting author sorting

--- books/books1.py
+++ books/books1.py
@@ -11,7 +11,6 @@ def readInput():
     if len(sys.argv) < 3:
         print("Please add to the command line a book source file and an action!", file=sys.stderr)
         print("Like this: python3 books1.py input-file action [sort-direction]", file=sys.stderr)
-        print("Git testing, Gus")
         sys.exit()
     cmd_line_arguments_list.append(sys.argv[1])
     cmd_line_arguments_list.append(sys.argv[2])
@@ -37,19 +36,61 @@ def getAction():
         sys.exit()
 
 
+def splitAuthors(author, authorList):
+    seperatedAuthors = []
+    loopcounter = 0
+    while(len(author) > 0 and loopcounter < 5):
+        loopcounter += 1
+        if(author.find(" and") == 0):
+            author = author[(author.find("and") + 4):]
+
+        seperatedAuthors.append(author[:author.find("(") - 1])
+        author = author[(author.find(")") + 1):]
+
+    if(loopcounter == 5):
+        print("infinite loop!!!!???")
+    for a in seperatedAuthors:
+        authorList.append(a)
+
+# Returns true if the first name preceds the other alphabetically
+
+
+def compareTwoNames(firstName, secondName):
+    firstLastName = firstName[(firstName.rfind(" ") + 1):]
+    secondLastName = secondName[(secondName.rfind(" ") + 1):]
+    return firstLastName < secondLastName
+
+
+def sortAuthors(unsortedList):
+    sortedList = []
+    for i in range(0, len(unsortedList)):
+        firstAlphabetical = "zzz"
+        for j in range(0, len(unsortedList)):
+            if(compareTwoNames(unsortedList[j], firstAlphabetical)):
+                firstAlphabetical = unsortedList[j]
+        sortedList.append(firstAlphabetical)
+        print("Adding " + firstAlphabetical)
+        unsortedList.remove(firstAlphabetical)
+    return sortedList
+
+
 def printAuthors():
     authors = []
-    authorsSpliced = []
+    authorsSplicedList = []
     with open(cmd_line_arguments_list[0], newline='') as f:
         reader = csv.reader(f)
         for row in reader:
             authors.append(row[2])
     for a in authors:
-        spliceIndex = a.find("(")
-        a = a[:spliceIndex]
-        authorsSpliced.append(a)
-    for author in authorsSpliced:
-        print(author)
+        if(a.find("and") == -1):
+            spliceIndex = a.find("(")
+            a = a[:spliceIndex]
+            authorsSplicedList.append(a)
+        else:
+            splitAuthors(a, authorsSplicedList)
+    sortedAuthorsList = sortAuthors(authorsSplicedList)
+    for a in sortedAuthorsList:
+        print(a)
 
 
 def printBooks():
