5155bf65d7cf2b92c0f374111c41241cdb786c02
Gustavo Diaz <diazg@res-231-171.dyn.carleton.edu>
2018-09-27 21:12:46 -0500

    Added user input from the command line into the code

--- using-apis/api-test.py
+++ using-apis/api-test.py
@@ -11,7 +11,7 @@ def get_teams():
     string_from_server = data_from_server.decode('utf-8')
     list_of_teams = json.loads(string_from_server)
 
-    print("Top Dota 2 teams according to Elo rating: ")
+    print("\nTop Dota 2 teams according to Elo rating: ")
     print("------------------------------------")
     print(list_of_teams[0]["name"])
     print("------------------------------------")
@@ -26,17 +26,32 @@ def get_player(player_id):
     data_from_server = urllib.request.urlopen(player_id).read()
     string_from_server = data_from_server.decode('utf-8')
     player_data = json.loads(string_from_server)
-    print("\'rtz\'s current matchmaking rating (MMR): \n" + str(player_data["solo_competitive_rank"]))
+    print("\n\'rtz\'s current matchmaking rating (MMR): \n" + str(player_data["solo_competitive_rank"]) +"\n\n")
 
 
 def print_time():
-    print("Accurate as of " + str(datetime.datetime.now()))
+    print("Accurate as of " + str(datetime.datetime.now()) + "\n")
+
+
+def get_user_command():
+    print("Here are the two operations available: ")
+    print("teams : gets the top 3 Dota 2 teams based on team Elo")
+    print("player : gets the solo elo of one the top Dota 2 platers in North America")
+    user_command = input("--> ")
+    return user_command
 
 
 def main():
-    get_teams()
-    get_player(86745912)
-    print_time()
+    function_to_run = get_user_command()
+    if(function_to_run == "teams"):
+        get_teams()
+        print_time()
+    elif(function_to_run == "player"):
+        get_player(86745912)
+        print_time()
+    else:
+        print("\nPlease only type in either 'teams' or 'player' as the argument!\n", file=sys.stderr)
+        sys.exit()
 
 
 if __name__ == '__main__':
