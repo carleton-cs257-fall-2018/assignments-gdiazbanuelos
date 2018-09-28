48f604c062433c0ec40f6104f1307a8e42596d3f
Gustavo Diaz <diazg@res-231-171.dyn.carleton.edu>
2018-09-27 19:57:07 -0500

    Added labels to the outputs of the pulls

--- using-apis/api-test.py
+++ using-apis/api-test.py
@@ -2,6 +2,7 @@ import sys
 import argparse
 import json
 import urllib.request
+import datetime
 
 
 def get_teams():
@@ -9,33 +10,34 @@ def get_teams():
     data_from_server = urllib.request.urlopen(teams_url).read()
     string_from_server = data_from_server.decode('utf-8')
     list_of_teams = json.loads(string_from_server)
+
+    print("Top Dota 2 teams according to Elo rating: ")
+    print("------------------------------------")
     print(list_of_teams[0]["name"])
     print("------------------------------------")
     print(list_of_teams[1]["name"])
     print("------------------------------------")
     print(list_of_teams[2]["name"])
+    print("------------------------------------\n\n")
 
 
 def get_player(player_id):
-    player_id = 'https://api.opendota.com/api/players/{0}'
-    player_id = player_id.format(player_id)
+    player_id = 'https://api.opendota.com/api/players/86745912'
     data_from_server = urllib.request.urlopen(player_id).read()
     string_from_server = data_from_server.decode('utf-8')
     player_data = json.loads(string_from_server)
-    print(player_data['leaderboard_rank'])
+    print("\'rtz\'s current matchmaking rating (MMR): \n" + str(player_data["solo_competitive_rank"]))
 
 
-
-def pull_match_data(match_id):
-    match_url = 'https://api.opendota.com/api/matches/{0}'
-    match_url = match_url.format(match_id)
-    print(match_url)
-
+def print_time():
+    print("Accurate as of " + str(datetime.datetime.now()))
 
 
 def main():
     get_teams()
     get_player(86745912)
+    print_time()
+
 
 if __name__ == '__main__':
     main()
