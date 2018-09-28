b5528d7f95180afc4cf969eafd8336b41b420095
Gustavo Diaz <diazg@res-231-171.dyn.carleton.edu>
2018-09-27 19:34:42 -0500

    Added new function to get player data

--- using-apis/api-test.py
+++ using-apis/api-test.py
@@ -3,18 +3,27 @@ import argparse
 import json
 import urllib.request
 
-'''
-base_url = 'https://api.opendota.com/api/'
-match_url = 'https://api.opendota.com/api/matches/{match_id}'
-player_url =  'https://api.opendota.com/api/players/{account_id}'
-'''
 
 def get_teams():
-    teams_url =  'https://api.opendota.com/api/teams/' 
+    teams_url = 'https://api.opendota.com/api/teams/'
     data_from_server = urllib.request.urlopen(teams_url).read()
     string_from_server = data_from_server.decode('utf-8')
     list_of_teams = json.loads(string_from_server)
-    print(len(list_of_teams))
+    print(list_of_teams[0]["name"])
+    print("------------------------------------")
+    print(list_of_teams[1]["name"])
+    print("------------------------------------")
+    print(list_of_teams[2]["name"])
+
+
+def get_player(player_id):
+    player_id = 'https://api.opendota.com/api/players/{0}'
+    player_id = player_id.format(player_id)
+    data_from_server = urllib.request.urlopen(player_id).read()
+    string_from_server = data_from_server.decode('utf-8')
+    player_data = json.loads(string_from_server)
+    print(player_data['leaderboard_rank'])
+
 
 
 def pull_match_data(match_id):
@@ -22,9 +31,11 @@ def pull_match_data(match_id):
     match_url = match_url.format(match_id)
     print(match_url)
 
+
+
 def main():
-    # pull_match_data(sys.argv[1])
     get_teams()
+    get_player(86745912)
 
 if __name__ == '__main__':
     main()
